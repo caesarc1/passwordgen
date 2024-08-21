76980a5a605d9ecc87579cdbb4e9ab8b160dca7f
caesarc1 <caiioliima95@gmail.com>
2024-08-20 22:50:12 -0300

    Finished de create_password function. A lot to do yet, but we're on the right track.

--- password_gen.py
+++ password_gen.py
@@ -1,18 +1,13 @@
-import random
-
 saved_passwords = {"Facebook": 123}
 
 def show_passwords():
     if not saved_passwords:
         print("You don't have any saved passwords.")
-
-        return 0
     else:
         print("Hi! These are your saved passwords:\n")
-
         for password in saved_passwords:
             print(password)
-    
+
         chosen_pass = input("\nWhich password do you want to show?\n")
         password = saved_passwords.get(chosen_pass)
         
@@ -21,18 +16,31 @@ def show_passwords():
         else:
             print("Password name not found.")
 
-create = show_passwords()
 
 def create_password():
-    if create == 0:
-        decision = input("Do you want to create a password?\nPress 1 to YES or 2 to NO\n")
-    else:
-        decision = input("Do you want to create another password?")
+    decision = input("Do you want to create a new password?\nPress 1 to YES or 2 to NO\n")
 
-    if decision != 1 or 2:
+    if decision not in ["1", "2"]:
         print("Invalid option")
+        return
+    
+    if decision == "2":
+        print("Ok!")
     else:
-        print("Ok")
+        new_name = input("How do you want to call your new password?\n")
+        new_pass = input("Ok! Now type the password.\n")
+
+        checking = input(f"The password is correct?\nPassword: {new_pass}\nPress 1 to YES or 2 to NO\n")
+
+        while checking == "2":
+            new_pass = input("Ok! Now type the correct password.\n")
+            checking = input(f"The password is correct?\nPassword: {new_pass}\nPress 1 to YES or 2 to NO\n")
+        
+        saved_passwords[new_name] = new_pass
+        print("Password saved!")
+
+show_passwords()
 
 create_password()
 
+show_passwords()
